c705ba86e75de45e5fc4ecef860ebf198c012f65
Abdelrahman <abdelrahman12312@outlook.com>
2025-06-07 20:12:06 +0300

    Update arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+# Arithmetic Operations Function
+
+def perform_operation(num1:float,num2:float,operation:str):
+  if operation == "add":
+      return num1+num2
+  elif operation == "subtract":
+      return num1-num2
+  elif operation == "multiply":
+        return num1*num2
+  elif operation == "divide":
+      if num2==0:
+          return "Error: Division by Zero"
+      return num1/num2
+  else:
+      return"Invalid numbers" 
