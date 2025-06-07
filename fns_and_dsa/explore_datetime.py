38fa00095812fc33d938a1463a6e0d43ef32de34
Abdelrahman <abdelrahman12312@outlook.com>
2025-06-07 21:11:19 +0300

    Create explore_datetime.py

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,13 @@
+import datetime
+
+year = datetime.datetime.now().year
+month = datetime.datetime.now().month
+day = datetime.datetime.now().day
+hour = datetime.datetime.now().hour
+minute = datetime.datetime.now().minute
+second = datetime.datetime.now().second
+
+
+def display_current_datetime():
+  print(f"{year}-{month}-{day} {hour}:{minute}:{second}")
+
